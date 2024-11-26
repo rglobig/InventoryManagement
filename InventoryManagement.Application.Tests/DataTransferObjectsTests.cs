05ac819651a3bce491279ddde9dc991a1eebb1b7
rene <rene>
2024-11-26 18:35:09 +0100

    added tests for DTOs and domain

---
+++ InventoryManagement.Application.Tests/DataTransferObjectsTests.cs
@@ -0,0 +1,35 @@
+﻿using InventoryManagement.Application.DataTransferObjects;
+using InventoryManagement.Domain;
+
+namespace InventoryManagement.Application.Tests;
+
+public class DataTransferObjectsTests
+{
+    [Fact]
+    void CreateInventoryItemDto_ToInventoryItem()
+    {
+        var input = new CreateInventoryItemDto("iPhone", "Smartphone", 1, 1000);
+
+        var item = input.ToInventoryItem();
+
+        Assert.NotNull(item);
+        Assert.Equal(input.Name, item.Name);
+        Assert.Equal(input.Description, item.Description);
+        Assert.Equal(input.Quantity, item.Quantity);
+        Assert.Equal(input.Price, item.Price);
+    }
+
+    [Fact]
+    void InventoryItemDto_FromInventoryItem()
+    {
+        var input = new InventoryItem("iPhone", "Smartphone", 1, 1000);
+
+        var item = InventoryItemDto.From(input);
+
+        Assert.NotNull(item);
+        Assert.Equal(input.Name, item.Name);
+        Assert.Equal(input.Description, item.Description);
+        Assert.Equal(input.Quantity, item.Quantity);
+        Assert.Equal(input.Price, item.Price);
+    }
+}
---
+++ InventoryManagement.Domain.Tests/InventoryItemTests.cs
@@ -0,0 +1,19 @@
+﻿namespace InventoryManagement.Domain.Tests;
+
+public class InventoryItemTests
+{
+    [Fact]
+    void InventoryItem_Update()
+    {
+        var item = new InventoryItem("iPhone", "Smartphone", 1, 1000);
+        var newName = "Pixel";
+        var newDescription = "Android";
+        var newQuantity = 2;
+        var newPrice = 800;
+        item.Update(newName, newDescription, newQuantity, newPrice);
+        Assert.Equal(newName, item.Name);
+        Assert.Equal(newDescription, item.Description);
+        Assert.Equal(newQuantity, item.Quantity);
+        Assert.Equal(newPrice, item.Price);
+    }
+}
